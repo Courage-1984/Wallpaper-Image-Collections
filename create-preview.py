d33094533860056fe44d98320d3331e39c7b4d57
DieterR97 <221122@virtualwindow.co.za>
2024-03-18 17:46:56 +0200

    adding Aesthetic Wallpapers

--- create-preview.py
+++ create-preview.py
@@ -5,7 +5,7 @@ def create_md_file(folder_path, md_file_path):
     with open(md_file_path, "w") as md_file:
         md_file.write("# Wallpapers in /{}\n\n".format(folder_name))
         for filename in os.listdir(folder_path):
-            if filename.endswith((".png", ".jpg", ".jpeg", ".gif")):
+            if filename.endswith((".png", ".jpg", ".jpeg", ".gif", ".webp")):
                 md_file.write(
                     "![](/{}/{})\n".format(
                         folder_name.replace(" ", "%20"), filename.replace(" ", "%20")
@@ -13,8 +13,8 @@ def create_md_file(folder_path, md_file_path):
                 )
 
 
-folder_name = "Gradient Wallpapers"
-md_file_name = "gradient-wallpapers-preview.md"
+folder_name = "Aesthetic Wallpapers"
+md_file_name = "aesthetic-wallpapers-preview.md"
 
 current_directory = os.getcwd()
 folder_path = os.path.join(current_directory, folder_name)
